595c63fb0641a52de3202647b837a9bf2c273ee1
Igor Savin <kibertoad@gmail.com>
2018-01-18 00:47:48 +0200

    Format with prettier

--- test/builder.spec.js
+++ test/builder.spec.js
@@ -2,131 +2,167 @@ const assert = require('chai').assert;
 const objectionBuilder = require('../index');
 
 describe('builder', () => {
-	it('equal', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.equal('field', 'value').build();
+  it('equal', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.equal('field', 'value').build();
 
-		assert.deepEqual({
-			'field:eq': 'value'
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        'field:eq': 'value'
+      },
+      params
+    );
+  });
 
-	it('gt', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.greaterThan('field', 5).build();
+  it('gt', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.greaterThan('field', 5).build();
 
-		assert.deepEqual({
-			'field:gt': 5
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        'field:gt': 5
+      },
+      params
+    );
+  });
 
-	it('lt', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.lessThan('field', 6).build();
+  it('lt', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.lessThan('field', 6).build();
 
-		assert.deepEqual({
-			'field:lt': 6
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        'field:lt': 6
+      },
+      params
+    );
+  });
 
-	it('gte', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.greaterThanOrEqual('field', 11).build();
+  it('gte', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.greaterThanOrEqual('field', 11).build();
 
-		assert.deepEqual({
-			'field:gte': 11
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        'field:gte': 11
+      },
+      params
+    );
+  });
 
-	it('lte', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.lessThanOrEqual('field', 22).build();
+  it('lte', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.lessThanOrEqual('field', 22).build();
 
-		assert.deepEqual({
-			'field:lte': 22
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        'field:lte': 22
+      },
+      params
+    );
+  });
 
-	it('in', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.in('field', [4, 9]).build();
+  it('in', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.in('field', [4, 9]).build();
 
-		assert.deepEqual({
-			'field:in': [4, 9]
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        'field:in': [4, 9]
+      },
+      params
+    );
+  });
 
-	it('single eager', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.eager('contacts').build();
+  it('single eager', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.eager('contacts').build();
 
-		assert.deepEqual({
-			'eager': 'contacts'
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        eager: 'contacts'
+      },
+      params
+    );
+  });
 
-	it('single array eager', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.eager(['contacts']).build();
+  it('single array eager', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.eager(['contacts']).build();
 
-		assert.deepEqual({
-			'eager': '[contacts]'
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        eager: '[contacts]'
+      },
+      params
+    );
+  });
 
-	it('multiple eager', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.eager(['contacts', 'address']).build();
+  it('multiple eager', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.eager(['contacts', 'address']).build();
 
-		assert.deepEqual({
-			'eager': '[contacts,address]'
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        eager: '[contacts,address]'
+      },
+      params
+    );
+  });
 
-	it('orderByAsc', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.orderByAsc('name').build();
+  it('orderByAsc', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.orderByAsc('name').build();
 
-		assert.deepEqual({
-			'orderBy': 'name'
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        orderBy: 'name'
+      },
+      params
+    );
+  });
 
-	it('orderByDesc', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder.orderByDesc('name').build();
+  it('orderByDesc', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder.orderByDesc('name').build();
 
-		assert.deepEqual({
-			'orderByDesc': 'name'
-		}, params)
-	});
+    assert.deepEqual(
+      {
+        orderByDesc: 'name'
+      },
+      params
+    );
+  });
 
+  it('between', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder
+      .greaterThan('field', 1)
+      .lessThan('field', 3)
+      .build();
 
+    assert.deepEqual(
+      {
+        'field:gt': 1,
+        'field:lt': 3
+      },
+      params
+    );
+  });
 
-	it('between', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder
-			.greaterThan('field', 1)
-			.lessThan('field', 3)
-			.build();
+  it('multiple', () => {
+    const builder = objectionBuilder.builder();
+    const params = builder
+      .greaterThan('field', 1)
+      .greaterThan('field2', 2)
+      .build();
 
-		assert.deepEqual({
-			'field:gt': 1,
-			'field:lt': 3
-		}, params)
-	});
-
-	it('multiple', () => {
-		const builder = objectionBuilder.builder();
-		const params = builder
-			.greaterThan('field', 1)
-			.greaterThan('field2', 2)
-			.build();
-
-		assert.deepEqual({
-			'field:gt': 1,
-			'field2:gt': 2
-		}, params)
-	});
-
+    assert.deepEqual(
+      {
+        'field:gt': 1,
+        'field2:gt': 2
+      },
+      params
+    );
+  });
 });
--- test/formatter.spec.js
+++ test/formatter.spec.js
@@ -7,41 +7,38 @@ const prefix = 'where';
 const assert = require('chai').assert;
 
 describe('formatter', () => {
+  it('simple, brackets', () => {
+    const result = ofq.format(data.simple, prefix, true);
+    assert.deepEqual(expected.simple, result);
+  });
 
-	it('simple, brackets', () => {
-		const result = ofq.format(data.simple, prefix, true);
-		assert.deepEqual(expected.simple, result);
-	});
+  it('simple, no brackets, no prefix', () => {
+    const result = ofq.format(data.simple, '', false);
+    assert.deepEqual(expected.simple_no_brackets, result);
+  });
 
-	it('simple, no brackets, no prefix', () => {
-		const result = ofq.format(data.simple, '', false);
-		assert.deepEqual(expected.simple_no_brackets, result);
-	});
+  it('simple, no prefix', () => {
+    const result = ofq.format(data.simple, '', true);
+    assert.deepEqual(expected.simple_no_prefix, result);
+  });
 
-	it('simple, no prefix', () => {
-		const result = ofq.format(data.simple, '', true);
-		assert.deepEqual(expected.simple_no_prefix, result);
-	});
+  it('composite', () => {
+    const result = ofq.format(data.composite);
+    assert.deepEqual(expected.composite, result);
+  });
 
-	it('composite', () => {
-		const result = ofq.format(data.composite);
-		assert.deepEqual(expected.composite, result);
-	});
+  it('multiple sibling', () => {
+    const result = ofq.format(data.multiple_sibling);
+    assert.deepEqual(expected.multiple_sibling, result);
+  });
 
-	it('multiple sibling', () => {
-		const result = ofq.format(data.multiple_sibling);
-		assert.deepEqual(expected.multiple_sibling, result);
-	});
+  it('array', () => {
+    const result = ofq.format(data.array);
+    assert.deepEqual(expected.array, result);
+  });
 
-	it('array', () => {
-		const result = ofq.format(data.array);
-		assert.deepEqual(expected.array, result);
-	});
-
-	it('nested', () => {
-		const result = ofq.format(data.nested);
-		assert.deepEqual(expected.nested, result);
-	});
+  it('nested', () => {
+    const result = ofq.format(data.nested);
+    assert.deepEqual(expected.nested, result);
+  });
 });
-
-
