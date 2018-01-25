e69af2effd9dcdc0d176ba7543f2fb56c519cd68
Igor Savin <isavin@lighthousenetwork.com>
2018-01-25 16:00:19 +0200

    Fix processing of empty values

--- test/eager.utils.spec.js
+++ test/eager.utils.spec.js
@@ -2,40 +2,48 @@ const eagerUtils = require('../lib/eager.utils');
 const assert = require('chai').assert;
 
 describe('eager.utils', () => {
-  it('removes single argument', () => {
-    const result = eagerUtils.removeFromEagerParam('[param1]', 'param1');
-    assert.equal(result, '[]');
-  });
-  it('removes single argument, ignores whitespace', () => {
-    const result = eagerUtils.removeFromEagerParam('[ param1 ]', 'param1');
-    assert.equal(result, '[]');
-  });
-  it('removes from multiple arguments', () => {
-    const result = eagerUtils.removeFromEagerParam('[param1,param2,param3]', 'param2');
-    assert.equal(result, '[param1,param3]');
-  });
-  it('removes multiple arguments', () => {
-    const result = eagerUtils.removeFromEagerParam('[param1,param2,param3,param4]', [
-      'param2',
-      'param1'
-    ]);
-    assert.equal(result, '[param3,param4]');
-  });
+	it('removes single argument', () => {
+		const result = eagerUtils.removeFromEagerParam('[param1]', 'param1');
+		assert.equal(result, '[]');
+	});
+	it('removes single argument, ignores whitespace', () => {
+		const result = eagerUtils.removeFromEagerParam('[ param1 ]', 'param1');
+		assert.equal(result, '[]');
+	});
+	it('removes from multiple arguments', () => {
+		const result = eagerUtils.removeFromEagerParam('[param1,param2,param3]', 'param2');
+		assert.equal(result, '[param1,param3]');
+	});
+	it('removes multiple arguments', () => {
+		const result = eagerUtils.removeFromEagerParam('[param1,param2,param3,param4]', [
+			'param2',
+			'param1'
+		]);
+		assert.equal(result, '[param3,param4]');
+	});
+	it('removes from empty param', () => {
+		const result = eagerUtils.removeFromEagerParam(undefined, 'param1');
+		assert.equal(result, '[]');
+	});
 
-  it('adds to single argument', () => {
-    const result = eagerUtils.appendToEagerParam('[param1]', 'param2');
-    assert.equal(result, '[param1,param2]');
-  });
-  it('adds to single argument, ignores whitespace', () => {
-    const result = eagerUtils.appendToEagerParam('[ param1 ]', 'param2');
-    assert.equal(result, '[param1,param2]');
-  });
-  it('adds to multiple arguments', () => {
-    const result = eagerUtils.appendToEagerParam('[param1, param3]', 'param2');
-    assert.equal(result, '[param1,param3,param2]');
-  });
-  it('adds multiple arguments', () => {
-    const result = eagerUtils.appendToEagerParam('[param1, param2]', ['param3', 'param4']);
-    assert.equal(result, '[param1,param2,param3,param4]');
-  });
+	it('adds to empty argument', () => {
+		const result = eagerUtils.appendToEagerParam(undefined, 'param2');
+		assert.equal(result, '[param2]');
+	});
+	it('adds to single argument', () => {
+		const result = eagerUtils.appendToEagerParam('[param1]', 'param2');
+		assert.equal(result, '[param1,param2]');
+	});
+	it('adds to single argument, ignores whitespace', () => {
+		const result = eagerUtils.appendToEagerParam('[ param1 ]', 'param2');
+		assert.equal(result, '[param1,param2]');
+	});
+	it('adds to multiple arguments', () => {
+		const result = eagerUtils.appendToEagerParam('[param1, param3]', 'param2');
+		assert.equal(result, '[param1,param3,param2]');
+	});
+	it('adds multiple arguments', () => {
+		const result = eagerUtils.appendToEagerParam('[param1, param2]', ['param3', 'param4']);
+		assert.equal(result, '[param1,param2,param3,param4]');
+	});
 });
